4e5a17bc3af03921fc2cd4ff8f1a4496e102c6ea
Rahul kumar maury <rahu7827759@gmail.com>
2024-08-12 14:15:32 +0530

    Form Validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { animate, motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
@@ -15,8 +15,40 @@ const iconVariants = (duration) => ({
 });
 
 const Contact = () => {
+  const formRef = useRef(null);
+  const [successMessage, setSucessMessage] = useState();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "bbc150f1-a0f8-4afe-bb12-e208469de2d3");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+      formRef.current.reset();
+      setSucessMessage("Form Submitted Successfully !");
+    }
+  };
+
   return (
-    <div className="border-b border-neutral-900 pb-20">
+    <form
+      ref={formRef}
+      onSubmit={onSubmit}
+      className="border-b border-neutral-900 pb-20"
+    >
       <h2 className="my-10 text-center text-4xl">Contact</h2>
       <motion.div
         whileInView={{ opacity: 1, x: 0 }}
@@ -28,14 +60,18 @@ const Contact = () => {
           <input
             className="w-3/4 p-4 text-black rounded-lg"
             type="text"
+            name="name"
             placeholder="Name"
+            required
           />
         </div>
         <div className="mb-8">
           <input
             className="w-3/4 p-4 text-black rounded-lg"
             type="text"
+            name="email"
             placeholder="Email"
+            required
           />
         </div>
         <div className="mb-8">
@@ -45,15 +81,28 @@ const Contact = () => {
             rows={8}
             cols={5}
             placeholder="Message..."
+            name="message"
+            required
           />
         </div>
         <div className="mb-8">
-          <button className="text-white bg-sky-500 px-8 py-2 rounded-lg hover:bg-sky-700 hover:px-9">
+          <button
+            type="submit"
+            className="text-white bg-sky-500 px-8 py-2 rounded-lg hover:bg-sky-700 hover:px-9"
+          >
             Submit
           </button>
         </div>
+        {successMessage && (
+          <div
+            className="text-3xl font-bold transition-transform "
+            style={{ marginTop: "20px", color: "green" }}
+          >
+            {successMessage}
+          </div>
+        )}
       </motion.div>
-    </div>
+    </form>
   );
 };
 
